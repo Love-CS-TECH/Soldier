78c720b78f25ec23ac71c32aaa2a92c0c239fe7e
Atul <royatul031@gmail.com>
2023-04-24 20:16:22 +0530

    Added electricity functionality

--- Building.java
+++ Building.java
@@ -3,6 +3,10 @@ public class Building {
         
     }
 
+    static void electricity() {
+        System.out.println("Electricity supply added");
+    }
+
     static void waterSupply() {
         System.out.println("water supply added");
     }
